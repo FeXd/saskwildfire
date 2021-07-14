70f2c1120c0b1f2594fbac003bb2b9c6c6ec3adb
Arlin Schaffel <arlin@fexd.com>
2021-07-14 16:45:40 -0600

    Saving PDF files as images
    * using `pdf2image` via `pip install pdf2image' & 'brew install poppler` (on macOS)
    * images are saving correctly and dpi can be adjusted with optional arguments

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import urllib.request
-import tweepy
-from dotenv import load_dotenv
+from pdf2image import convert_from_path
 
+fire_pdfs = [
+    'http://environment.gov.sk.ca/firefiles/activefires.pdf',
+    'http://environment.gov.sk.ca/firefiles/MunicipalFireBans.pdf',
+    'http://environment.gov.sk.ca/firefiles/DailyFireDangerMaps/today_fwi.pdf',
+    'http://environment.gov.sk.ca/firefiles/DailyFireDangerMaps/tomorrow_fwi.pdf',
+]
+
+local_pdfs = [
+    './pdf/activefires.pdf',
+    './pdf/MunicipalFireBans.pdf',
+    './pdf/today_fwi.pdf',
+    './pdf/tomorrow_fwi.pdf',
+]
 
 if __name__ == '__main__':
-    print('hello world')
+    for pdf_file in local_pdfs:
+        images = convert_from_path(pdf_file)
+        for i, image in enumerate(images):
+            fname = pdf_file+str(i)+'.jpg'
+            image.save(fname, "JPEG")
