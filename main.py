a647780ebad5dc5bee72989b1f228377ecf5d77d
Arlin Schaffel <arlin@fexd.com>
2021-07-15 09:31:28 -0600

    Download PDFs and Save Images

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import requests
 from pdf2image import convert_from_path
 
+fire_url = 'http://environment.gov.sk.ca/firefiles/'
+
 fire_pdfs = [
-    'http://environment.gov.sk.ca/firefiles/activefires.pdf',
-    'http://environment.gov.sk.ca/firefiles/MunicipalFireBans.pdf',
-    'http://environment.gov.sk.ca/firefiles/DailyFireDangerMaps/today_fwi.pdf',
-    'http://environment.gov.sk.ca/firefiles/DailyFireDangerMaps/tomorrow_fwi.pdf',
+    'activefires.pdf',
+    'MunicipalFireBans.pdf',
+    'DailyFireDangerMaps/today_fwi.pdf',
+    'DailyFireDangerMaps/tomorrow_fwi.pdf',
 ]
 
-local_pdfs = [
-    './pdf/activefires.pdf',
-    './pdf/MunicipalFireBans.pdf',
-    './pdf/today_fwi.pdf',
-    './pdf/tomorrow_fwi.pdf',
-]
+
+def download_files(url, path, files):
+    for file in files:
+        response = requests.get(url+file, stream=True)
+        write_file = open(path+file, 'wb')
+        write_file.write(response.content)
+        write_file.close()
+
+
+def generate_images_from_pdfs(in_path, pdfs, out_path):
+    for pdf in pdfs:
+        images = convert_from_path(in_path+pdf)
+        for i, image in enumerate(images):
+            fname = pdf+str(i)+'.jpg'
+            image.save(out_path+fname, "JPEG")
+
 
 if __name__ == '__main__':
-    for pdf_file in local_pdfs:
-        images = convert_from_path(pdf_file)
-        for i, image in enumerate(images):
-            fname = pdf_file+str(i)+'.jpg'
-            image.save(fname, "JPEG")
+    download_files(fire_url, './pdf/', fire_pdfs)
+    generate_images_from_pdfs('./pdf/', fire_pdfs, './image/')
